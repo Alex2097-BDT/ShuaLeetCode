8895d8545d03c49250cb1ccf8fd352e8af68a23a
Yueming Yu <55822574+alex2097-bdt@users.noreply.github.com>
2019-10-07 17:23:42 -0700

    impl a simple test engine

--- Solutions/[1]Two_Sum/[1][2]Two_Sum.cpp
+++ Solutions/[1]Two_Sum/[1][2]Two_Sum.cpp
@@ -1,28 +1,7 @@
 //
 // Created by alex on 10/7/19.
 //
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include "TestEngine.h"
-using namespace std;
 
-TestEngine *TestEngine::_test_engine = nullptr;
-TestEngine::TestEngine() = default;
-TestEngine::~TestEngine() {
-    delete _test_engine;
-}
-TestEngine::TestEngine(const TestEngine &) = default;
-TestEngine& TestEngine::operator=(const TestEngine &) = default;
-TestEngine& TestEngine::instance() {
-    static TestEngine _test_engine = TestEngine();
-    return _test_engine;
-}
-
-
-/**
- * paste Solution here
- */
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -62,22 +41,3 @@ public:
         return ret;
     }
 };
-
-/**
- * write test function here
- * @return
- */
-RC TestEngine::test(){
-    Solution solution;
-    vector<int> input_1 = {20,6,3,10};
-    int input_2 = 13;
-    vector<int> output(solution.twoSum(input_1, input_2));
-    print_vector(output);
-    return SUCCESS;
-}
-
-int main(int argc, char *argv[]){
-    TestEngine &testEngine = TestEngine::instance();
-    testEngine.test();
-    return SUCCESS;
-}
